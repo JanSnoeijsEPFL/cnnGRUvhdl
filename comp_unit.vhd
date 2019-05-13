b8b6491123e57b355da8e8f4d9d1fd5ed0146bd1
JanSnoeijsEPFL <jan.snoeijs@epfl.ch>
2019-05-13 11:31:27 +0200

    fixed rounding error in hard sigmoid function

--- comp_unit.vhd
+++ comp_unit.vhd
@@ -76,11 +76,9 @@ architecture rtl of comp_unit is
 		end if;
 	end process;
 	
-	hard_sigm: process(xtrunc)
-	begin
-		hs_out <= std_logic_vector(shift_right(signed(xtrunc), 2)+8) when xtrunc(1) = '0' or when (xtrunc(2) = '0' and xtrunc(1 downto 0) = "10") else
-					std_logic_vector(shift_right(signed(xtrunc), 2)+9); -- the shift right operation implies another truncation
-	end process;
+
+	hs_out <= std_logic_vector(shift_right(signed(xtrunc), 2)+8) when xtrunc(1) = '0' or (xtrunc(2) = '0' and xtrunc(1 downto 0) = "10") else
+				std_logic_vector(shift_right(signed(xtrunc), 2)+9); -- the shift right operation implies another truncation
 	
 end architecture;
  
