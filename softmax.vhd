7a0252418086a4234caf0436aeb676b766df2a88
JanSnoeijsEPFL <jan.snoeijs@epfl.ch>
2019-05-23 15:28:56 +0200

    added extra clock cycle for division

--- softmax.vhd
+++ softmax.vhd
@@ -20,9 +20,10 @@ entity softmax is
 end entity softmax;
 
 architecture rtl of softmax is
-	type FSM is (sleep, calc_sum, divide);
+	type FSM is (sleep, calc_sum, divide_1, divide_2);
 	signal state_reg, state_next : FSM;
 	signal state_1_reg, state_1_next : FSM;
+	signal state_2_reg, state_2_next : FSM;
 	type exp_arr is array(0 to OUTPUTS-1) of std_logic_vector(NBITS_DIV+FRAC_LUT-1 downto 0);
 	type out_arr is array(0 to OUTPUTS-1) of std_logic_vector(NBITS_DIV-1 downto 0);
 	signal acc_reg, acc_next : std_logic_vector(NBITS_DIV-1 downto 0);
@@ -41,13 +42,14 @@ architecture rtl of softmax is
 	signal softmax_rdy_reg, softmax_rdy_next : std_logic;
 	signal x_new : std_logic_vector(NBITS-1 downto 0);
 	signal y_reg, y_next : out_arr;
-	signal div_res : std_logic_vector(NBITS_DIV+FRAC_LUT-1 downto 0)
+	signal div_res : std_logic_vector(NBITS_DIV+FRAC_LUT-1 downto 0);
 	signal x_reg, x_next : std_logic_vector(NBITS*OUTPUTS-1 downto 0);
 	--signal trig_softmax_1_reg, trig_softmax_1_next : std_logic;
 begin
 	
 	-- reg chain
 	state_1_next <= state_reg;
+	state_2_next <= state_1_reg;
 	exp_Cntr_1_next <= exp_CntrVal;
 	exp_Cntr_2_next <= exp_Cntr_1_reg;
 	--trig_softmax_1_next <= trig_softmax;
@@ -59,6 +61,7 @@ begin
 			sum_reg <= (others => '0');
 			state_reg <= sleep;
 			state_1_reg <= sleep;
+			state_2_reg <= sleep;
 			softmax_rdy_reg <= '0';
 			exp_Cntr_1_reg <= (others => '0');
 			exp_Cntr_2_reg <= (others => '0');
@@ -71,6 +74,7 @@ begin
 			sum_reg <= sum_next;
 			state_reg <= state_next;
 			state_1_reg <= state_1_next;
+			state_2_reg <= state_2_next;
 			softmax_rdy_reg <= softmax_rdy_next;
 			exp_Cntr_1_reg <= exp_Cntr_1_next;
 			exp_Cntr_2_reg <= exp_Cntr_2_next;
@@ -85,13 +89,11 @@ begin
 		state_next <= state_reg;
 		exp_CntrEnable <= '0';
 		exp_CntrReset <= '0';
-
 		case state_reg is
 			when sleep =>
 				if trig_softmax = '1' then
 					state_next <= calc_sum;
 					exp_CntrReset <= '1';
-					div_CntrReset <= '1';
 				end if;
 			when calc_sum => 
 				exp_CntrEnable <= '1';
@@ -104,6 +106,8 @@ begin
 				exp_CntrEnable <= '1';
 				if exp_CntrEnd = '1' then
 					state_next <= sleep;
+				else
+					state_next <= divide_1;
 				end if;
 		end case;
 	end process;
@@ -118,15 +122,15 @@ begin
 		);
 	x_next <= x when trig_softmax = '1' else x_reg;	
 	x_new <= x_reg(NBITS*to_integer(unsigned(exp_CntrVal)) +NBITS-1 downto NBITS*to_integer(unsigned(exp_CntrVal)));
-	softmax_rdy_next <= '1' when state_1_reg = divide and exp_CntrEnd = '1' else '0';
+	softmax_rdy_next <= '1' when state_1_reg = divide_2 and exp_CntrEnd = '1' else '0';
 	softmax_rdy <= softmax_rdy_reg;
 	acc_extended <= DIV_ZEROS&acc_reg;
 	reg_fill: for i in 0 to OUTPUTS-1 generate
 		exp_next(i) <=exp_out& DIV_ZEROS when state_reg = calc_sum and to_integer(unsigned(exp_CntrVal))=i else exp_reg(i);
 		y_next(i) <= div_res(NBITS_DIV-1 downto 0) when state_2_reg = divide_2 and to_integer(unsigned(exp_Cntr_2_reg)) = i else y_reg(i);
-		y(NBITS*i+NBITS-1 downto NBITS*i) <= y_next(i);
+		y(NBITS_DIV*i+NBITS_DIV-1 downto NBITS_DIV*i) <= y_next(i);
 	end generate;
-	div_res <= std_logic_vector(unsigned(exp_reg(to_integer(unsigned(exp_Cntr_2_reg)))/(unsigned(acc_extended))) 
+	div_res <= std_logic_vector(unsigned(exp_reg(to_integer(unsigned(exp_Cntr_2_reg))))/(unsigned(acc_extended)))
 					when state_2_reg = divide_2 else (others => '0');
 	sum_next <= exp_out when state_reg = calc_sum else sum_reg;
 	acc_next <= std_logic_vector(resize(unsigned(sum_reg), acc_reg'length)+unsigned(acc_reg)) when state_1_reg = calc_sum else acc_reg;
